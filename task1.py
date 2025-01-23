52e4a38d205b51fdbda6976300592dd0b1a5b0c3
Ellie Pearson <ellie.jm.pearson@gmail.com>
2025-01-22 17:22:17 -0800

    task2 submit

--- task1.py
+++ task1.py
@@ -1,60 +1,62 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
-def main():
+# encrypt_image takes a file (string), mode (string), and potentially keys (strings) and creates an encrypted file using the given mode, and key
+# Returns nothing but outputs an encrypted bmp file
+def encrypt_image(img_filename, mode, key=None, iv=None):
+    with open(img_filename, 'rb') as file:
+        header = file.read(54)
+        plaintext = file.read() #read rest of the image data
 
-    # encrypt_image takes a file (string), mode (string), and potentially keys (strings) and creates an encrypted file using the given mode and key, returns nothing
-    def encrypt_image(img_filename, mode, key=None, iv=None):
-        with open(img_filename, 'rb') as file:
-            header = file.read(54)
-            plaintext = file.read() #read rest of the image data
+    #add padding
+    padded_plaintext = pad(plaintext)
 
-        # padding
-        padding_size = 16 - (len(plaintext) % 16) #compute # padding bytes
-        padding = bytes([padding_size] * padding_size) # creates padding_size bytes of value padding_size
-        padded_plaintext = padding + plaintext
+    # following block given by Prof.
+    # Encrypt based on mode
+    if key is None:
+        key = get_random_bytes(16)
 
-        # following block given by Prof.
-        # Encrypt based on mode
-        if key is None:
-            key = get_random_bytes(16)
+    if mode == 'ECB':
+        output = header + ecb_encrypt(padded_plaintext, key)
+    elif mode == 'CBC':
+        if iv is None:
+            iv = get_random_bytes(16)
+        output = header + cbc_encrypt(padded_plaintext, key, iv)
+    else:
+        raise NotImplementedError("Mode not implemented")
 
-        cipher = AES.new(key, AES.MODE_ECB) #user MODE_ECB for both because MODE_CBC automatically xors it
-        ciphertext = bytes(0)
+    # write encrypted date to file
+    with open(f"{img_filename.replace('.bmp', '')}_{mode}_encrypted.bmp", "wb") as file:
+        file.write(output)
 
-        if mode == 'ECB':
-            output = header + ecb_encrypt(padded_plaintext, cipher, ciphertext)
-        elif mode == 'CBC':
-            if iv is None:
-                iv = get_random_bytes(16)
-            output = header + cbc_encrypt(padded_plaintext, iv, cipher, ciphertext)
-        else:
-            raise NotImplementedError("Mode not implemented")
+    print("encrypted image", img_filename, "in mode", mode)
 
-        # write encrypted date to file
-        with open(f"{img_filename.replace('.bmp', '')}_{mode}_encrypted.bmp", "wb") as file:
-            file.write(output)
+# takes plaintext (string) and adds padding using PKCS#7 method
+def pad(plaintext):
+    padding_size = 16 - (len(plaintext) % 16) #compute # padding bytes
+    padding = bytes([padding_size] * padding_size) # creates padding_size bytes of value padding_size
+    return plaintext + padding
 
-        print("encrypted image", img_filename, "in mode", mode)
+# takes plaintext (bytes), a cipher, and ciphertext (bytes) and returns plaintext encrypted in ECB mode
+def ecb_encrypt(plaintext, key):
+    cipher = AES.new(key, AES.MODE_ECB)  # user MODE_ECB for both because MODE_CBC automatically xors it
+    ciphertext = bytes(0)
+    for i in range(0, len(plaintext), 16):
+        ciphertext += cipher.encrypt(plaintext[i:i + 16]) #encrypt one block
+    return ciphertext
 
-    # takes plaintext (bytes), a cipher, and ciphertext (bytes) and returns plaintext encrypted in ECB mode
-    def ecb_encrypt(plaintext, cipher, ciphertext):
-        for i in range(0, len(plaintext), 16):
-            ciphertext += cipher.encrypt(plaintext[i:i + 16]) #encrypt one block
-        return ciphertext
+# takes plaintext (bytes), an iv, a cipher, and ciphertext (bytes) and returns plaintext encrypted in CBC mode
+def cbc_encrypt(plaintext, key, iv):
+    cipher = AES.new(key, AES.MODE_ECB)  # user MODE_ECB for both because MODE_CBC automatically xors it
+    ciphertext = bytes(0)
+    prev_block = iv
+    for i in range(16, len(plaintext), 16):
+        block = plaintext[i:i + 16]
+        xor_block = bytes(a ^ b for a, b in zip(block, prev_block)) #xor bytes to prev_block
+        ciphertext += cipher.encrypt(xor_block)
+        prev_block = xor_block
+    return ciphertext
 
-    # takes plaintext (bytes), an iv, a cipher, and ciphertext (bytes) and returns plaintext encrypted in CBC mode
-    def cbc_encrypt(plaintext, iv, cipher, ciphertext):
-        prev_block = iv
-        for i in range(16, len(plaintext), 16):
-            block = plaintext[i:i + 16]
-            xor_block = bytes(a ^ b for a, b in zip(block, prev_block)) #xor bytes to prev_block
-            ciphertext += cipher.encrypt(xor_block)
-            prev_block = xor_block
-        return ciphertext
-
-    # test
-    encrypt_image('cp-logo.bmp', 'CBC')
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    #test
+    encrypt_image('cp-logo.bmp', 'ECB')
--- task2.py
+++ task2.py
@@ -2,34 +2,25 @@ from Crypto.Random import get_random_bytes
 import urllib.parse
 import task1
 
+key = get_random_bytes(16)
+iv = get_random_bytes(16)
 
-def main():
-    key = get_random_bytes(16)
-    iv = get_random_bytes(16)
+# Submit function takes in a string and returns an encoded string with added data
+def submit(input_txt):
+    encoded_txt = urllib.parse.quote(input_txt)
+    full_msg = f"userid=456;userdata={encoded_txt};session-id=31337"
+    msg_bytes  = full_msg.encode() #convert message to bytes to add padding
+    padded_txt = pad(msg_bytes)
+    encrypted = task1.cbc_encrypt(padded_txt, key, iv)
+    return encrypted
 
-    # Submit function takes in a string and returns a string with added data
-    # take an arbitrary string provided by the user, and prepend the string: "userid=456; userdata="
-    # and append the string: ";session-id=31337"
-    # Also:
-    # (1) URL encode any ‘;’ and ‘=’ characters that appear in the user provided string;
-    # (2) pad the final string(using PKCS # 7), and
-    # (3) encrypt the padded string using the AES - 128 - CBC you implemented in Task  1
-    def submit(input_txt):
-        encoded_txt = urllib.parse.quote(input_txt)
-        print(type(encoded_txt))
-        print(encoded_txt)
-        padded_txt = pad(bytes(encoded_txt, "utf-8"))
-        print(padded_txt)
-        encrypted = task1.cbc_encrypt(padded_txt, key, iv)
-        return encrypted
+# takes plaintext (string) and adds padding using PKCS#7 method
+def pad(message):
+    padding_length = 16 - (len(message) % 16)
+    padding = bytes([padding_length] * padding_length)
+    return message + padding
 
-    # takes plaintext (string) and adds padding using PKCS#7 method
-    def pad(message):
-        padding_length = 16 - (len(message) % 16)
-        padding = bytes([padding_length] * padding_length)
-        return message + padding
-
-    submit("You’re the man; now=dog")
+submit("You’re the man; now=dog")
 
 if __name__ == '__main__':
-    main()
+    print(submit("You’re the man; now=dog"))
